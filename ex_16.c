baa215097a7a9f44121b31989bcd561c44b16711
Gabriely Di Folco Rocha <al.gabriely.rocha@impatech.org.br>
2025-05-29 16:29:09 -0300

    Update ex_16.c

--- ex_16.c
+++ ex_16.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int V[5];
+    float V[5];
     int code;
 
     for(int i = 0; i<5; ++i){
-        scanf("%d", &V[i]);
+        scanf("%f", &V[i]);
     }
 
     scanf("%d", &code);
 
     if(code == 1){
         for(int i = 0; i<5; ++i){
-            printf("\n %d", V[i]);
+            printf("\n %f", V[i]);
         }
     }
     else if(code == 2){
         for(int i = 4; i>=0; --i){
-            printf("\n %d", V[i]);
+            printf("\n %f", V[i]);
         }
 
     }
