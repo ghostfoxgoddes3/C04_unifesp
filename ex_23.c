1f74932788e8582d7628371e40181344bdd0c9fd
Gabriely Di Folco Rocha <al.gabriely.rocha@impatech.org.br>
2025-05-29 16:31:52 -0300

    Update ex_23.c

--- ex_23.c
+++ ex_23.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main(){
-    int X[5], Y[5];
-    int dot_p = 0;
+    float X[5], Y[5];
+    float dot_p = 0;
 
     for(int i = 0; i<5; ++i){
-        scanf("%d", &X[i]);
+        scanf("%f", &X[i]);
     }
     for(int i = 0; i<5; ++i){
-        scanf("%d", &Y[i]);
+        scanf("%f", &Y[i]);
     }
 
     for(int i = 0; i<5; ++i){
-        dot.p += X[i]*Y[i];
+        dot_p += X[i]*Y[i];
     }
 
     printf("\n");
     for(int i = 0; i<5; ++i){
-        printf("%d \t", X[i]);
+        printf("%f \t", X[i]);
     }
     printf("\n");
     for(int i = 0; i<5; ++i){
-        printf("\n %d", Y[i]);
+        printf("\n %f", Y[i]);
     }
-    printf("\n %d", dot_p);
+    printf("\n %f", dot_p);
 
 }
