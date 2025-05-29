750c79f1ec95b8710646eab9167a723e37105910
Gabriely Di Folco Rocha <al.gabriely.rocha@impatech.org.br>
2025-05-29 16:51:00 -0300

    Update ex_27.c

--- ex_27.c
+++ ex_27.c
@@ -2,16 +2,24 @@
 
 int main(){
     int V[10];
+    int isprime[10];
 
     for(int i = 0; i < 10; ++i){
         scanf("%d", &V[i]);
-        for(int j = 2; j < V[i]; ++j){
+        if(V[i] < 2){
+            isprime[i] = 0;
+        }
+        for(int j = 2; j < sqrt(V[i]); ++j){
             if(V[i]%j != 0){
+                isprime[i] = 0;
                 break
             }
-            // buguei aqui, vai ver é o horário
+            isprime[i] = 1;
         }
-
     }
-
+    for(int i = 0; i<10; i++){
+        if(isprime[i] == 1){
+            printf("element %d position %d", V[i], i);
+        }
+    }
 }
